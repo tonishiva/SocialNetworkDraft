54773b3ae3d3638c9f6a6b22a3de1a5cfe2ca9ad
tonishiva <63733561+tonishiva@users.noreply.github.com>
2020-06-06 01:20:19 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 const menuBtn = document.querySelector('.m-menu');
 const menuSidebar = document.querySelector('.menu-sidebar');
-menuBtn.addEventListener('click', function() {
+
+menuBtn.addEventListener('click', event => {
     menuSidebar.classList.toggle('active');
+});
+
+const menuCollapse = () => {
+    menuSidebar.classList.remove('active');
+};
+
+document.body.addEventListener('click', event => {
+
+    if (!event.target.closest('.m-menu') && !event.target.closest('.menu-sidebar')) {
+        menuCollapse();
+    }
 });
