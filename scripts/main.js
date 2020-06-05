bb5008f3a6631650e7e59c00072a6a1a11ea3ab6
tonishiva <63733561+tonishiva@users.noreply.github.com>
2020-06-05 22:34:04 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.m-menu');
+const menuSidebar = document.querySelector('.menu-sidebar');
+menuBtn.addEventListener('click',
+    event => {
+        menuSidebar.classList.toggle('active');
+    })
